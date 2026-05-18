94074e17359fa48180c4afd80c3a529d762ff797
Ana-gon03 <lupita.ortiz.0311@gmail.com>
2026-05-18 06:55:41 -0600

    fix: add missing useEffect import in VerificarCorreoLogin

--- frontend/src/pages/publicas/VerificarCorreoLogin.jsx
+++ frontend/src/pages/publicas/VerificarCorreoLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import NavbarSimple from '../../components/common/NavbarSimple'
 import FooterInicio from '../../components/common/FooterInicio'
